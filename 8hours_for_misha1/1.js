28e0c426cd0512a9f032122564e0adb4e1f76e55
Unute <jhfgjsk@gmail.com>
2025-08-10 18:30:24 +0300

    first commit

---
+++ 8hours_for_misha1/1.js
@@ -0,0 +1,28 @@
+// function hello(name){
+//   let phrase = `Hello, ${name}`;
+//   debugger;
+//   say(phrase);
+// }
+
+// function say(phrase){
+//   alert(`** ${phrase} **`);
+// }
+
+// hello('john');
+
+// for(let i = 1; i <= 5; i++){
+//   console.log(i);
+// }
+
+
+// документирование
+/**
+ * Возвращает x, возведённое в n-ную степень.
+ *
+ * @param {number} x Возводимое в степень число.
+ * @param {number} n Степень, должна быть натуральным числом.
+ * @return {number} x, возведённое в n-ную степень.
+ */
+function pow(x, n) {
+  ...
+}
